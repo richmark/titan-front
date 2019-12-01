86c00f1449fadb2dd288b9b8c0c93f7c0f7470d2
badjinn <richmark.jinn.ravina@gmail.com>
2019-12-01 11:13:09 +0800

    feat: removed public/resources
    removed unnecessary package on package.json

--- src/core/admin/DashboardLayout.js
+++ src/core/admin/DashboardLayout.js
@@ -2,11 +2,10 @@ import React, { Fragment } from "react";
 import Menu from "./Menu";
 import Navbar from "./Navbar";
 import MainContent from "./MainContent";
-import "../../resources/bootstrap/vendor/fontawesome-free/css/all.min.css";
-import "../../resources/bootstrap/css/sb-admin-2.min.css";
+import '../../front/styles/sb-admin-2.min.css';
 
 const DashboardLayout = ({ name, detail, children }) => {
-  return (
+  return (    
     <Fragment>
       <div id="wrapper">
         <Menu />
--- src/index.js
+++ src/index.js
@@ -2,7 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../src/front/styles/sb-admin-2.min.css';
-
 ReactDOM.render(<Routes />, document.getElementById('root'));
