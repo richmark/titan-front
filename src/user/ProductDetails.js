ca230d2360cb4cf90cd98d7cbeaf5256617f5e1e
Carlo <cbarcena20@gmail.com>
2019-12-09 22:28:38 +0800

    feat: dynamic badge for cart

--- src/user/ProductDetails.js
+++ src/user/ProductDetails.js
@@ -6,12 +6,14 @@ import { Container, Row, Col, Image, Form, Button } from "react-bootstrap";
 import { getProduct } from "../core/admin/products/productsApi";
 import { getCategory } from "../core/admin/categories/categoriesApi";
 import { IMAGE_API } from "../config";
+import { addItem, getTotalCount } from '../core/client/cartHelpers';
 
 const ProductDetails = ({match}) => {
   
   const [oProduct, setProduct] = useState({
     image : "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRQGvHazjKHOSITUSvJC1CUOSWGBZKYbMiEYNZHn5sg007KcVhS",
     product_name: "My Product",
+    _id: match.params.productId,
     price: '',
     description: ''
   });
@@ -21,9 +23,17 @@ const ProductDetails = ({match}) => {
     name: ''
   });
 
+  const [iRun, setRun] = useState(getTotalCount());
   const [iCount, setCount] = useState(1);
 
   const [bProduct, setBoolProduct] = useState(true);
+
+  const addToCart = () => {
+    addItem(oProduct, iCount, () => {
+      alert('Item added!');
+      setRun(getTotalCount())
+    });
+  };
 
   const init = () => {
     getProduct(match.params.productId).then(oData => {
@@ -32,6 +42,7 @@ const ProductDetails = ({match}) => {
         setBoolProduct(false);
       } else {
         setProduct({
+          ...oProduct,
           image : `${IMAGE_API}/images/products/${oData.image}`,
           product_name: oData.product_name,
           price: oData.price,
@@ -61,7 +72,7 @@ const ProductDetails = ({match}) => {
   }, []);
 
   const handleCount = oEvent => {
-    setCount(oEvent.target.value < 1 ? 1 : oEvent.target.value);
+    setCount(oEvent.target.value < 1 ? 1 : parseInt(oEvent.target.value, 10));
   };
 
   const checkProduct = bProduct => {
@@ -114,7 +125,7 @@ const ProductDetails = ({match}) => {
               </Form>
               <hr />
               <Button variant="primary">Buy Now</Button>{" "}
-              <Button variant="primary">Add to Cart</Button>
+              <Button variant="primary" onClick={addToCart}>Add to Cart</Button>
             </Col>
           </Row>
         </Container>
@@ -147,7 +158,7 @@ const ProductDetails = ({match}) => {
   };
 
   return (
-    <Layout title="ProductDetails" description="Sign up here">
+    <Layout run={iRun}>
       {showProductMain()}
       {showDetails()}
       {showRelatedProduct()}
