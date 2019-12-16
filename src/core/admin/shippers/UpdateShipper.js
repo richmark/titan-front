128f9b1f841072ede73b243c1cab8b2ccc831919
richmark <richmark.jinn.ravina@gmail.com>
2019-12-17 06:15:15 +0800

    feat: shipper CRUD

---
+++ src/core/admin/shippers/UpdateShipper.js
@@ -0,0 +1,134 @@
+import React, { Fragment, useState, useEffect } from 'react';
+import DashboardLayout from '../DashboardLayout';
+import { isAuthenticated } from '../../../auth/authUtil';
+import { getShipper, updateShipper } from './shippersApi';
+
+const UpdateShipper = ({ match }) => {
+
+    const {sToken, user} = isAuthenticated();
+    const [values, setValues] = useState({
+        shipper_name: '',
+        contact_person: '',
+        contact_number: '',
+        shipper_address: ''
+    });
+    const {
+        shipper_name,
+        contact_person,
+        contact_number,
+        shipper_address
+    } = values;
+
+    const handleChange = name => oEvent => {
+        setValues({ ...values, [name]: oEvent.target.value });
+    }
+
+    const loadShipper = () => {
+        getShipper(user._id, sToken, match.params.shipperId).then(oData => {
+            if (oData.error) {
+                console.log(oData.error);
+            } else {
+                setValues(oData.data);
+            }
+        });
+    };
+
+    const submitShipper = oEvent => {
+        oEvent.preventDefault();
+        updateShipper(user._id, sToken, match.params.shipperId, values).then(oData => {
+            if (oData.error) {
+                console.log(oData.error);
+            } else {
+                alert('Updated Successfully');
+            }
+        });
+    }
+
+    useEffect(() => {
+        loadShipper();
+    }, []);
+
+    const showUpdateShipper = () => {
+        return (
+            <Fragment>
+                <div className='col-md-4 col-sm-4 col-xl-4 mb-4'>
+                    <div className='card border-left-primary shadow h-100 py-2'>
+                        <div className='card-body'>
+                            <div className='form-group row'>
+                                <label
+                                    htmlFor='inputPassword'
+                                    className='col-sm-4 col-form-label'
+                                >
+                                    Shipper Name
+                                </label>
+                                <div className='col-sm'>
+                                    <input
+                                        value={shipper_name}
+                                        onChange={handleChange('shipper_name')}
+                                        type='text'
+                                        className='form-control'
+                                        id='inputPassword'
+                                    />
+                                </div>
+                            </div>
+                            <div className='form-group row'>
+                                <label
+                                    htmlFor='inputPassword'
+                                    className='col-sm-4 col-form-label'
+                                >
+                                    Contact Person
+                                </label>
+                                <div className='col-sm'>
+                                    <input
+                                        value={contact_person}
+                                        onChange={handleChange('contact_person')}
+                                        type='text'
+                                        className='form-control'
+                                        id='inputPassword'
+                                    />
+                                </div>
+                            </div>
+                            <div className='form-group row'>
+                                <label
+                                    htmlFor='inputPassword'
+                                    className='col-sm-4 col-form-label'
+                                >
+                                    Telephone
+                                </label>
+                                <div className='col-sm'>
+                                    <input
+                                        value={contact_number}
+                                        onChange={handleChange('contact_number')}
+                                        type='text'
+                                        className='form-control'
+                                        id='inputPassword'
+                                    />
+                                </div>
+                            </div>
+                            <div className='form-group row'>
+                                <label
+                                    htmlFor='inputPassword'
+                                    className='col-sm-4 col-form-label'
+                                >
+                                    Address
+                                </label>
+                                <div className='col-sm'>
+                                    <textarea value={shipper_address} onChange={handleChange('shipper_address')} className='form-control w-100' />
+                                </div>
+                            </div>
+                            <button onClick={submitShipper} className='btn btn-primary'>Update</button>
+                        </div>
+                    </div>
+                </div>
+            </Fragment>
+        );
+    };
+
+    return (
+        <DashboardLayout name='Shipper Management' detail='Update Shipper'>
+            {showUpdateShipper()}
+        </DashboardLayout>
+    );
+};
+
+export default UpdateShipper;
---
+++ src/core/admin/shippers/shippersApi.js
@@ -0,0 +1,61 @@
+import { API_URL } from '../../../config';
+
+export const getAllShippers = (sId, sToken) => {
+    return fetch(`${API_URL}/shippers/${sId}`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${sToken}`
+        }
+    })
+    .then(response => {
+        return response.json();
+    })
+    .catch(err => console.log(err));
+};
+
+export const createShipper = (sId, sToken, oData) => {
+    return fetch(`${API_URL}/shipper/create/${sId}`, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${sToken}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(oData)
+    })
+    .then(response => {
+        return response.json();
+    })
+    .catch(err => console.log(err));
+}
+
+export const getShipper = (sId, sToken, shipperId) => {
+    return fetch(`${API_URL}/shippers/${sId}/${shipperId}`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${sToken}`
+        }
+    })
+    .then(response => {
+        return response.json();
+    })
+    .catch(err => console.log(err)); 
+}
+
+export const updateShipper = (sId, sToken, shipperId, oData) => {
+    return fetch(`${API_URL}/shippers/${sId}/${shipperId}`, {
+        method: 'PUT',
+        headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${sToken}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(oData)
+    })
+    .then(response => {
+        return response.json();
+    })
+    .catch(err => console.log(err));
+};
