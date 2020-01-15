9b0052e80843d70cbf34d13cd5173a3719e12cda
Carlo <cbarcena20@gmail.com>
2020-01-15 09:43:38 +0800

    feat: can now send review with validation

--- src/user/ReviewProduct.js
+++ src/user/ReviewProduct.js
@@ -1,16 +1,22 @@
 import React, { useState, useEffect, Fragment } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import Layout from '../core/Layout';
 import { Container, Row, Col, Card, ProgressBar, Table, Image, Form, Button  } from 'react-bootstrap';
 import { IMAGE_API } from '../config';
 import { getProduct } from '../core/admin/products/productsApi';
 import { isAuthenticated } from '../auth/authUtil';
+import { createReview } from '../core/admin/reviews/reviewsApi';
+import { oValidatorLibrary } from '../libraries/validatorLibrary';
+import BasicAlert from './format/BasicAlert';
 
 const ProductReview = ({ match }) => {
 
     const [iStar, setStar] = useState(5);
     const [oProduct, setProduct] = useState(false);
+    const [mAlert, setAlert] = useState(false);
+    const [bRedirect, setRedirect] = useState(false);
 
+    const sOrderId = match.params.orderId;
     const { sToken, user } = isAuthenticated();
 
     useEffect(() => {
@@ -23,6 +29,49 @@ const ProductReview = ({ match }) => {
             }
         });
     }, []);
+
+    const submitReview = () => {
+        setAlert(false);
+        var oValidator = oValidatorLibrary();
+        const oData = {
+            rate: iStar,
+            comment: getValue('commentArea')
+        }
+        oValidator.message('comment', oData.comment, 'required');
+        
+        if (oValidator.allValid()) {
+            sendReview(oData);
+        } else {
+            setAlert(oValidator.getErrorMessages().comment);
+        }
+    }
+
+    const redirectUser = () => {
+        if (bRedirect === true) {
+            return <Redirect to={`/order/detail/${sOrderId}`}/>
+        }
+    }
+
+    const sendReview = (oParam) => {
+        createReview(user._id, sToken, oParam, oProduct._id, sOrderId).then(oData => {
+            if (oData.error) {
+                console.log(oData.error);
+            } else {
+                alert('Review submitted!')
+                setRedirect(true);
+            }
+        });
+    }
+
+    const showAlert = () => {
+        if (mAlert !== false) {
+            return BasicAlert('danger', mAlert);
+        }
+    }
+
+    const getValue = (sValue) => {
+        return document.getElementById(sValue).value.trim()
+    }
 
     const showStar = () => {
         const iRating = 5;
@@ -55,14 +104,13 @@ const ProductReview = ({ match }) => {
     }
 
     const showProductReview = () => {
-        return (
+        return oProduct && (
             <Container className="mt-5">
                 <h2>Write Review</h2>
                 <Card className="mt-2">
                     <Row>
                         <Col sm={{span: 6}}>
                             <Container className="m-2">
-                               <strong>Date Delivered:</strong> <span>08/12/19</span><br/>
                                <strong>Rate and Review Product</strong>
                             </Container>
                         </Col>
@@ -81,12 +129,13 @@ const ProductReview = ({ match }) => {
                         </div>
                         {showRating()}
                         <div className="mt-4">
-                            <Form.Group controlId="exampleForm.ControlTextarea1">
+                            <Form.Group controlId="commentArea">
                                 <Form.Control as="textarea" rows="3" />
                             </Form.Group>
                         </div>
                         <div>
-                            <Button variant="warning">Submit Review</Button>
+                            {showAlert()}
+                            <Button variant="warning" onClick={submitReview}>Submit Review</Button>
                         </div>
                     </Container>
                 </Card>
@@ -97,6 +146,7 @@ const ProductReview = ({ match }) => {
 	return (
         <Layout>
             {showProductReview()}
+            {redirectUser()}
         </Layout>
     );
 };
