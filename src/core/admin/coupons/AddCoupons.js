056e2a6e0b9e7250eecccb287afb5101121322a0
Jon <jjaguilar08@gmail.com>
2020-01-20 22:16:49 +0800

    finished add coupon functionality

--- src/core/admin/coupons/AddCoupons.js
+++ src/core/admin/coupons/AddCoupons.js
@@ -7,43 +7,20 @@ import { Redirect } from "react-router-dom";
 
 const AddCoupons = () => {
   const { sToken, user } = isAuthenticated();
-  const [sMessage, setMessage] = useState("");
+  const [aMessages, setMessages] = useState([]);
 
   const [values, setValues] = useState({
     coupon_name: "",
-    coupon_name_error: false,
     coupon_code: "",
-    coupon_code_error: false,
     coupon_type: "Discount Rate",
     description: "",
-    description_error: false,
     discount: "",
-    discount_error: false,
     start_date: "",
-    start_date_error: false,
     end_date: "",
-    end_date_error: false,
-    error: false,
     status: true
   });
 
-  const {
-    coupon_name,
-    coupon_name_error,
-    coupon_code,
-    coupon_code_error,
-    coupon_type,
-    description,
-    description_error,
-    discount,
-    discount_error,
-    start_date,
-    start_date_error,
-    end_date,
-    end_date_error,
-    status,
-    error
-  } = values;
+  const { coupon_code } = values;
 
   const generateCode = iLength => {
     var sResult = "";
@@ -60,85 +37,64 @@ const AddCoupons = () => {
 
   const validateData = aData => {
     if (aData.coupon_name === "") {
-      setError("Coupon name is required!", {
-        ...values,
-        coupon_name_error: true,
-        error: true
-      });
+      alert("Coupon name is required.");
       return false;
     }
 
     if (aData.coupon_name.length > 32) {
-      setError("Coupon name can only contain up to 32 characters!", {
-        ...values,
-        coupon_name_error: true,
-        error: true
-      });
+      alert("Coupon name can only contain 32 characters.");
       return false;
     }
 
     if (aData.coupon_code.length > 24) {
-      setError("Coupon code can only contain up to 24 characters!", {
-        ...values,
-        coupon_code_error: true,
-        error: true
-      });
+      alert("Coupon code can only contain 24 characters");
       return false;
     }
 
     if (aData.coupon_code === "") {
-      setError("Coupon code is required!", {
-        ...values,
-        coupon_code_error: true,
-        error: true
-      });
+      alert("Coupon code is required.");
       return false;
     }
 
     if (aData.description === "") {
-      setError("Description is required!", {
-        ...values,
-        description_error: true,
-        error: true
-      });
+      alert("Description is required.");
       return false;
     }
 
     if (aData.description.length > 50) {
-      setError("Description can only contain up to 50 characters!", {
-        ...values,
-        description_error: true,
-        error: true
-      });
+      alert("Description can only contain 50 characters.");
       return false;
     }
 
     if (aData.discount < 0) {
-      setError("Discount can not be negative!", {
-        ...values,
-        discount_error: true,
-        error: true
-      });
+      alert("Discount can not be a negative value.");
       return false;
     }
 
     if (aData.discount === "") {
-      setError("Discount is required!", {
-        ...values,
-        discount_error: true,
-        error: true
-      });
+      alert("Discount is required.");
+      return false;
+    }
+
+    if (
+      new Date(aData.start_date).getTime() > new Date(aData.end_date).getTime()
+    ) {
+      alert("End date can not be earlier than start date.");
+      return false;
+    }
+
+    if (aData.start_date === "") {
+      alert("Start date is required.");
+      return false;
+    }
+
+    if (aData.end_date === "") {
+      alert("End date is required.");
       return false;
     }
 
     return true;
   };
-
-  const setError = (sMessage, aData) => {
-    setMessage(sMessage);
-    setValues(aData);
-  };
-
   const handleChange = sName => oEvent => {
     const value = oEvent.target.value;
     setValues({ ...values, [sName]: value });
@@ -148,8 +104,7 @@ const AddCoupons = () => {
     if (validateData(values)) {
       checkCouponCode(values.coupon_code).then(oData => {
         if (oData.data.length !== 0) {
-          setMessage("Coupon code must be unique!");
-          setValues({ ...values, coupon_name_error: true, error: true });
+          alert("Coupon code already exists.");
           return false;
         }
         createCoupon(user._id, sToken, values).then(oData => {
@@ -157,6 +112,7 @@ const AddCoupons = () => {
             alert("Something went wrong. Please try again");
           } else {
             alert("Coupon created successfully");
+            window.location.reload();
           }
         });
       });
@@ -164,15 +120,10 @@ const AddCoupons = () => {
   };
 
   const handleGenerateCode = oEvent => {
-    setValues({ ...values, coupon_code: generateCode(8) });
-  };
-
-  const showErrorDiv = sMessage => {
-    return (
-      <div className="alert alert-danger">
-        <h6>{sMessage}</h6>
-      </div>
-    );
+    setValues({
+      ...values,
+      coupon_code: generateCode(8)
+    });
   };
 
   const showAddCouponForm = () => {
@@ -183,7 +134,6 @@ const AddCoupons = () => {
             <div className="col-md-6 col-sm-6 col-xl-6 mb-4">
               <div className="card border-left-primary shadow h-100 py-2">
                 <div className="card-body">
-                  {error ? showErrorDiv(sMessage) : ""}
                   <input
                     type="text"
                     onChange={handleChange("coupon_name")}
