cd5cf864c37082aa05666cf45357515bbb6864f4
Joven <jovenpaja@gmail.com>
2020-01-28 20:45:39 +0800

    feat: added ratings

--- src/user/format/comment/CommentCard.js
+++ src/user/format/comment/CommentCard.js
@@ -10,16 +10,32 @@ const CommentCard = () => {
             <Card className="mb-3">
                 <Card.Body>
                     <Card.Title>Sample Name</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">Verified Purchase</Card.Subtitle>
+                    <Card.Subtitle className="mb-2 text-muted">
+                        Verified Purchase 
+                        <span className="ml-2">
+                            <span className="fa fa-star checked"></span>
+                            <span className="fa fa-star checked"></span>
+                            <span className="fa fa-star checked"></span>
+                            <span className="fa fa-star checked"></span>
+                            <span className="fa fa-star checked"></span>
+                        </span>
+                    </Card.Subtitle>
                     <Card.Text>
-                    Will Order again, masarap sya, lasang yummy
+                    Will Order again, best one yet.
                     </Card.Text>
                 </Card.Body>
             </Card>
             <Card>
                 <Card.Body>
                     <Card.Title>Sample Name</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">Verified Purchase</Card.Subtitle>
+                    <Card.Subtitle className="mb-2 text-muted">Verified Purchase
+                        <span className="ml-2">
+                            <span className="fa fa-star checked"></span>
+                            <span className="fa fa-star checked"></span>
+                            <span className="fa fa-star checked"></span>
+                            <span className="fa fa-star checked"></span>
+                            <span className="fa fa-star checked"></span>
+                        </span></Card.Subtitle>
                     <Card.Text>
                     Will Order again, malakas yung makina, pang malakasan
                     </Card.Text>
