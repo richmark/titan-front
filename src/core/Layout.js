3f3239b86f8525d16c0f1c08afb2cf8493037de5
Jon <jjaguilar08@gmail.com>
2020-01-31 20:45:00 +0800

    fixed merge conflicts

--- src/core/Layout.js
+++ src/core/Layout.js
@@ -64,7 +64,6 @@ const Layout = ({
     window.location.replace(searchUrl);
   };
 
-
   const splice = aCategories => {
     if (aCategories.length > 10) {
       var aTempCatA = JSON.parse(JSON.stringify(aCategories));
@@ -73,58 +72,8 @@ const Layout = ({
       var aSecondHalf = aTempCatB.splice(
         aTempCatB.length / 2,
         aTempCatB.length
-      ); 
+      );
       return { aFirst: aFirstHalf, aSecond: aSecondHalf };
-
-    const showUserGuest = () => {
-        if (user) {
-            return (
-                <Fragment>
-                    <div className="container" style={{display: 'flex', flexWrap : 'wrap'}}>
-                        <Col className="mt-2 mb-2 ellipsis pl-0 pr-0" style={{maxWidth: '170px'}}>
-                            <span className="text-white"><i className="far fa-envelope" style={{color: '#ffc044'}}></i> admin@titan.com</span>
-                        </Col>
-                        <Col className="mt-2 mb-2 ellipsis pl-0 pr-0">
-                            <span className="text-white"><i className="fas fa-phone-alt" style={{color: '#ffc044'}}></i> 028-00000</span>
-                        </Col>
-                        <Col xs={6} md={6} xl={6} sm={6} className="mt-2 mb-2 text-right ellipsis">
-                            <a href={`/profile/${user._id}`} className="text-white"><i className="fas fa-user" style={{color: '#ffc044'}}></i> My Profile</a>
-                        </Col>
-                        <Col xs={1} md={1} xl={1} sm={1} className="mt-2 mb-2 text-right ellipsis pr-0">
-                            <a href="/signout" className="text-white"><i className="fas fa-sign-out-alt" style={{color: '#ffc044'}}></i> Logout</a>
-                        </Col>
-                    </div>
-                </Fragment>
-            );  
-        }
-        return (
-            <Fragment>
-                <div className="container" style={{display: 'flex', flexWrap : 'wrap'}}>
-                    <Col className="mt-2 mb-2 ellipsis pl-0 pr-0" style={{maxWidth: '170px'}}>
-                        <span className="text-white"><i className="far fa-envelope" style={{color: '#ffc044'}}></i> admin@titan.com</span>
-                    </Col>
-                    <Col className="mt-2 mb-2 ellipsis pl-0 pr-0">
-                        <span className="text-white"><i className="fas fa-phone-alt" style={{color: '#ffc044'}}></i> 028-00000</span>
-                    </Col>
-                    <Col xs={6} md={6} xl={6} sm={6} className="mt-2 mb-2 text-right ellipsis">
-                    <a href="/login" className="text-white"><i className="fas fa-user" style={{color: '#ffc044'}}></i> Login</a>
-                    </Col>
-                    <Col xs={2} md={2} xl={2} sm={2} className="mt-2 mb-2 ellipsis pr-0" style={{maxWidth: '102px'}}>
-                    <a href="/signup" className="text-white" style={{right: '0'}}><span style={{
-                        backgroundImage: `url(${IMAGE_API}/images/others/register.png)`,
-                        backgroundPosition: '6px -2px',
-                        backgroundSize: '20px',
-                        backgroundRepeat: 'no-repeat',
-                        height: '22px',
-                        width: '24px',
-                        display: 'block',
-                        cursor: 'pointer',
-                        paddingLeft: '28px'
-                    }}>Register</span></a>
-                    </Col>  
-                </div>
-            </Fragment>
-        );
     }
     return aCategories;
   };
@@ -243,22 +192,22 @@ const Layout = ({
             className="mt-2 mb-2 ellipsis pr-0"
             style={{ maxWidth: "102px" }}
           >
-            <a href="/signup" className="text-white" style={{ right: "0" }}>
-              <span
-                style={{
-                  backgroundImage: `url(${IMAGE_API}/images/others/ICons.png)`,
-                  backgroundPosition: "75px -67px",
-                  backgroundSize: "47px",
-                  height: "22px",
-                  width: "24px",
-                  display: "block",
-                  cursor: "pointer",
-                  paddingLeft: "28px",
-                  marginRight: "30px"
-                }}
-              >
-                Register
-              </span>
+            <a
+              href="/signup"
+              className="text-white"
+              style={{
+                backgroundImage: `url(${IMAGE_API}/images/others/register.png)`,
+                backgroundPosition: "6px -2px",
+                backgroundSize: "20px",
+                backgroundRepeat: "no-repeat",
+                height: "22px",
+                width: "24px",
+                display: "block",
+                cursor: "pointer",
+                paddingLeft: "28px"
+              }}
+            >
+              Register
             </a>
           </Col>
         </div>
@@ -394,8 +343,6 @@ const Layout = ({
               >
                 Categories
               </Dropdown.Toggle>
-
-<<<<<<< HEAD
               {showNavCategories()}
             </Dropdown>
           </Col>
@@ -443,58 +390,6 @@ const Layout = ({
       </Navbar>
     );
   };
-=======
-    const showNavBarThird = () => {
-        return (
-            <Nav activeKey="/home" className="container">
-                <Nav.Item>
-                    <Nav.Link style={{textDecoration: 'none', color: 'black'}} href="/"><span style={{
-                        backgroundImage: `url(${IMAGE_API}/images/others/ICons.png)`,
-                        backgroundPosition: '125px -152px',
-                        backgroundSize: '60px',
-                        height: '22px',
-                        width: '24px',
-                        display: 'block',
-                        cursor: 'pointer',
-                        paddingLeft: '28px',
-                        marginRight: '30px'
-                        }}><strong>Home</strong>
-                    </span></Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link style={{textDecoration: 'none', color: 'black'}} href="/"><span style={{
-                        backgroundImage: `url(${IMAGE_API}/images/others/ICons.png)`,
-                        backgroundPosition: '132px -134px',
-                        backgroundSize: '65px',
-                        height: '22px',
-                        width: '24px',
-                        display: 'block',
-                        cursor: 'pointer',
-                        paddingLeft: '28px',
-                        marginRight: '30px'
-                    }}><strong>Shop</strong></span></Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link style={{textDecoration: 'none', color: 'black'}} href={user && `/profile/${user._id}`}>
-                        <Row className="ml-1">
-                        <span style={{
-                        backgroundImage: `url(${IMAGE_API}/images/others/order.png)`,
-                        backgroundPosition: '6px 0px',
-                        backgroundSize: '18px',
-                        backgroundRepeat: 'no-repeat',
-                        height: '22px',
-                        width: '24px',
-                        display: 'block',
-                        cursor: 'pointer',
-                        paddingLeft: '28px'
-                        }}></span><strong>Your Order</strong></Row>
-                    </Nav.Link>
-                </Nav.Item>
-            </Nav>
-        );
-    };
->>>>>>> master
-
   const showNavBarThird = () => {
     return (
       <Nav activeKey="/home" className="container">
@@ -544,9 +439,10 @@ const Layout = ({
             <Row className="ml-1">
               <span
                 style={{
-                  backgroundImage: `url(${IMAGE_API}/images/others/ICons.png)`,
-                  backgroundPosition: "100px -74px",
-                  backgroundSize: "50px",
+                  backgroundImage: `url(${IMAGE_API}/images/others/order.png)`,
+                  backgroundPosition: "6px 0px",
+                  backgroundSize: "18px",
+                  backgroundRepeat: "no-repeat",
                   height: "22px",
                   width: "24px",
                   display: "block",
