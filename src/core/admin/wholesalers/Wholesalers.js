c1418006d691907b25ef0779a2fc8cb012884763
richmark <richmark.jinn.ravina@gmail.com>
2020-04-13 19:03:37 +0800

    fix: get all wholesaler as sub admin

--- src/core/admin/wholesalers/Wholesalers.js
+++ src/core/admin/wholesalers/Wholesalers.js
@@ -2,7 +2,8 @@ import React, { Fragment, useEffect, useState } from 'react';
 import { getAllWholesalers } from './wholesalersApi';
 import { isAuthenticated } from '../../../auth/authUtil';
 import DashboardLayout from '../DashboardLayout';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import DataTable from "react-data-table-component";
 
 const Wholesalers = () => {
     const { sToken, user } = isAuthenticated();
@@ -20,68 +21,55 @@ const Wholesalers = () => {
         init();
     }, []);
     const showWholesalers = () => {
+        const oData = wholesalers;
+        const oColumns = [
+            {
+                name: "Company Name",
+                selector: "company_name",
+                sortable: true,
+                cell: oRow => {
+                    return (
+                      <Fragment>
+                        <Link to={`wholesalers/${oRow._id}`}>
+                            {oRow.company_name}
+                        </Link>
+                      </Fragment>
+                    );
+                }
+            },
+            {
+                name: "Status",
+                selector: "verified_admin",
+                sortable: true,
+                cell: oRow => {
+                    return (
+                        <Fragment>
+                            {oRow.verified_admin ?  'Verified' : 'Pending'}
+                        </Fragment>
+                    )
+                }
+            }
+        ];
+
         return (
             <Fragment>
                 <div className='col-md-12 col-sm-12 col-xl-12 mb-4'>
                     <div className='card border-left-primary shadow h-100 py-2'>
                         <div className='card-body'>
-                            <select
-                                id='category'
-                                className='btn btn-light border mr-2'
-                            >
-                                <option disabled defaultValue>
-                                    Filter by level
-                                </option>
-                                <option>Bronze</option>
-                                <option>Silver</option>
-                                <option>Gold</option>
-                            </select>
-                            <select
-                                id='category'
-                                className='btn btn-light border mr-2'
-                            >
-                                <option disabled defaultValue>
-                                    Filter by status
-                                </option>
-                                <option>Processing</option>
-                                <option>verified</option>
-                            </select>
-                            <button className='btn btn-primary'>Filter</button>
-                        </div>
-                    </div>
-                </div>
-                <div className='col-md-12 col-sm-12 col-xl-12 mb-4'>
-                    <div className='card border-left-primary shadow h-100 py-2'>
-                        <div className='card-body'>
-                            <div className='float-right'>
-                                <span>10</span> Items
-                            </div>
-                            <table className='table table-bordered'>
-                                <thead>
-                                    <tr>
-                                        <th scope='col'>Company Name</th>
-                                        <th scope='col'>Status</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {wholesalers.map((oData, iIndex) => (
-                                        <tr key={iIndex}>
-                                            <td>
-                                                <Link to={`wholesalers/${oData._id}`}>
-                                                    {oData.company_name}
-                                                </Link>
-                                            </td>
-                                            <td>{oData.verified_admin ?  'Verified' : 'Pending'}</td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
+                            <DataTable
+                                columns={oColumns}
+                                data={oData}
+                                pagination={true}
+                                striped
+                                keyField='_id'
+                            />
                         </div>
                     </div>
                 </div>
             </Fragment>
         );
     };
+
     return (
         <DashboardLayout
             name='Wholesale User Application'
