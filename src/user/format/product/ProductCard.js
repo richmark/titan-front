6f6ff4a367820b24d276240826e9699108aab8ad
Carlo <cbarcena20@gmail.com>
2020-07-10 17:40:39 +0800

    design: added google fonts roboto and oswald

--- src/user/format/product/ProductCard.js
+++ src/user/format/product/ProductCard.js
@@ -56,7 +56,7 @@ const ProductCard = (aData, setRun = () => {}, sName = 'OUR PRODUCTS') => {
                         </a>
                     </Col>
                 </Row>
-                <Row style={{ fontFamily: 'Roboto Condensed', fontWeight: 'bold'}}>
+                <Row style={{ fontFamily: 'Roboto Condensed, sans-serif', fontWeight: 'bold'}}>
                     <Col sm={{offset : 1, span : 12}} style={{fontSize: '.7rem'}}>
                         <p style={{width: '80%'}} className='text-truncate'>{sName}</p>
                     </Col>
@@ -83,10 +83,10 @@ const ProductCard = (aData, setRun = () => {}, sName = 'OUR PRODUCTS') => {
         return (
             <Row className="mt-3 mx-2 productCardHide">
                 <Col lg={5} className='px-0 my-1 mx-1 text-center' style={{border : '1px solid rgba(0,0,0,.125)', borderRadius : '.25rem'}}>
-                    <Button onClick={buyNow(oProduct)} variant="" style={{ fontWeight : 'bold',fontSize: '.9rem', color : '#ff6900', fontFamily : 'Oswald Bold'}}>Buy Now</Button>
+                    <Button onClick={buyNow(oProduct)} variant="" style={{ fontWeight : 'bold',fontSize: '.9rem', color : '#ff6900', fontFamily : 'Oswald, sans-serif'}}>Buy Now</Button>
                 </Col>
                 <Col lg={5} className='px-0 my-1 mx-1 text-center' style={{border: '1px solid #ff6900', backgroundColor: '#ff6900', borderRadius : '.25rem'}}>
-                    <Button className='border-0' onClick={oClick} style={{fontSize: '.9rem', backgroundColor: 'transparent',fontFamily : 'Oswald Bold'}}>Add to Cart</Button>
+                    <Button className='border-0' onClick={oClick} style={{fontSize: '.9rem', backgroundColor: 'transparent',fontFamily : 'Oswald, sans-serif'}}>Add to Cart</Button>
                 </Col>
             </Row>
         );
@@ -174,7 +174,7 @@ const ProductCard = (aData, setRun = () => {}, sName = 'OUR PRODUCTS') => {
 
     return (
         <Container>
-            <div className="category-tab mt-3" style={{background: `url(${IMAGE_API}/images/others/CategoryTab.png) no-repeat 0 0`, height: '85px'}}><strong><p className="mb-0 absolute" style={{position: 'relative', top: '14px', left: '60px', fontSize : '20px', fontFamily : 'Oswald Bold', fontWeight : 'bold'}}>{sHeader}</p></strong></div>
+            <div className="category-tab mt-3" style={{background: `url(${IMAGE_API}/images/others/CategoryTab.png) no-repeat 0 0`, height: '85px'}}><strong><p className="mb-0 absolute" style={{position: 'relative', top: '14px', left: '60px', fontSize : '20px', fontFamily : 'Oswald, sans-serif', fontWeight : 'bold'}}>{sHeader}</p></strong></div>
             {showLayout(aData)}
         </Container>
     );    
